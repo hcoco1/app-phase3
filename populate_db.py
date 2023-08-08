3ac2a4b8899195326fee49f39f4395595f340765
Ivan <arias.ivan@gmail.com>
2023-08-08 02:53:39 -0400

    first state added

--- populate_db.py
+++ populate_db.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import click
-from sqlalchemy import create_engine, Column, Integer, String, Float, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, String, Float
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -20,42 +20,62 @@ class State(Base):
     Capital = Column(String)
     Area = Column(Float)
 
-class County(Base):
-    __tablename__ = 'counties'
-    CountyID = Column(Integer, primary_key=True)
-    CountyName = Column(String)
-    Population = Column(Integer)
-    Area = Column(Float)
-    StateID = Column(Integer, ForeignKey('states.StateID'))
-
-class City(Base):
-    __tablename__ = 'cities'
-    CityID = Column(Integer, primary_key=True)
-    CityName = Column(String)
-    Population = Column(Integer)
-    Latitude = Column(Float)
-    Longitude = Column(Float)
-    StateID = Column(Integer, ForeignKey('states.StateID'))
-    CountyID = Column(Integer, ForeignKey('counties.CountyID'))
-
-class CityCounty(Base):
-    __tablename__ = 'citycounties'
-    CityCountyID = Column(Integer, primary_key=True)
-    CityID = Column(Integer, ForeignKey('cities.CityID'))
-    CountyID = Column(Integer, ForeignKey('counties.CountyID'))
-
 @click.group()
 def cli():
     pass
 
 @cli.command()
-def initdb():
-    """Create the database tables."""
-    Base.metadata.create_all(engine)
-    click.echo('Initialized the database.')
+@click.argument('name')
+@click.argument('abbreviation')
+@click.argument('population', type=int)
+@click.argument('capital')
+@click.argument('area', type=float)
+def create_state(name, abbreviation, population, capital, area):
+    """Create a new state."""
+    new_state = State(
+        StateName=name,
+        Abbreviation=abbreviation,
+        Population=population,
+        Capital=capital,
+        Area=area
+    )
+    session.add(new_state)
+    session.commit()
+    click.echo(f'State "{name}" created.')
+
+@cli.command()
+def read_states():
+    """Read and display all states."""
+    states = session.query(State).all()
+    for state in states:
+        click.echo(f'{state.StateID}: {state.StateName} ({state.Abbreviation})')
+
+@cli.command()
+@click.argument('state_id', type=int)
+@click.argument('new_name')
+def update_state(state_id, new_name):
+    """Update a state's name."""
+    state = session.query(State).get(state_id)
+    if state:
+        state.StateName = new_name
+        session.commit()
+        click.echo(f'State {state_id} updated with new name: {new_name}.')
+    else:
+        click.echo(f'State with ID {state_id} not found.')
+
+@cli.command()
+@click.argument('state_id', type=int)
+def delete_state(state_id):
+    """Delete a state."""
+    state = session.query(State).get(state_id)
+    if state:
+        session.delete(state)
+        session.commit()
+        click.echo(f'State {state_id} deleted.')
+    else:
+        click.echo(f'State with ID {state_id} not found.')
 
 if __name__ == '__main__':
     cli()
-
 
 
