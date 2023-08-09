1f0067bbf629ee3995c3dd3bd44841879fc05d80
Ivan <arias.ivan@gmail.com>
2023-08-09 11:52:05 -0400

    color added to the input choices

--- user_interaction.py
+++ user_interaction.py
@@ -3,6 +3,7 @@ import display
 from database_operations import session
 from models import State, City, County
 import datetime
+from termcolor import colored
 
 
 
@@ -29,20 +30,20 @@ def save_user_details():
 
 def get_user_query():
     while True:
-        print("\nChoose an operation:")
-        print("1. Show the DataBase")
-        print("2. Add a new State")
-        print("3. Add a new City")
-        print("4. Add a new County")
-        print("5. Update a State")
-        print("6. Update a City")
-        print("7. Update a County")
-        print("8. Delete a State")
-        print("9. Delete a County")
-        print("10. Delete a City")
-        print("11. Exit")
+        print(colored("\nChoose a CRUD operation:", "red"))
+        print(colored("1. Show the DataBase", "yellow"))
+        print(colored("2. Add a new State", "yellow"))
+        print(colored("3. Add a new City", "yellow"))
+        print(colored("4. Add a new County", "yellow"))
+        print(colored("5. Update a State", "yellow"))
+        print(colored("6. Update a City", "yellow"))
+        print(colored("7. Update a County", "yellow"))
+        print(colored("8. Delete a State", "yellow"))
+        print(colored("9. Delete a County", "yellow"))
+        print(colored("10. Delete a City", "yellow"))
+        print(colored("11. Exit", "yellow"))
 
-        choice = input("Enter your choice: ")
+        choice = input(colored("Enter your choice: ", "red"))
 
         if choice == "1":
             display.display_states(session)
@@ -50,48 +51,48 @@ def get_user_query():
             display.display_cities(session)
 
         elif choice == "2":
-            state_name = input("Enter state name: ").strip().lower()
+            state_name = input(colored("Enter state name: ", "blue")).strip().lower()
             database_operations.add_single_state(session, state_name.title())
 
         elif choice == "3":
-            state_name = input("Enter the name of the state where the city is located: ").strip().lower()
-            county_name = input("Enter the name of the county where the city is located: ").strip().lower()
-            name = input("Enter city name: ").strip().lower()
+            state_name = input(colored("Enter the name of the state where the city is located: ", "blue")).strip().lower()
+            county_name = input(colored("Enter the name of the county where the city is located: ", "blue")).strip().lower()
+            name = input(colored("Enter city name: ", "blue")).strip().lower()
             database_operations.add_single_city(session, name.title(), state_name.title(), county_name.title() )
 
         elif choice == "4":
-            state_name = input("Enter the name of the state where the county is located: ").strip().lower()
-            name = input("Enter county name: ").strip().lower()
+            state_name = input(colored("Enter the name of the state where the county is located: ", "blue")).strip().lower()
+            name = input(colored("Enter county name: ", "blue")).strip().lower()
             database_operations.add_single_county(session,name.title(), state_name.title() )
             
         elif choice == "5":
-            state_name = input("Enter the name of the state to modify: ").strip().lower()
-            attribute = input("Enter the attribute to modify: ").strip().lower()
-            new_value = input("Enter the new value: ").strip().lower()
+            state_name = input(colored("Enter the name of the state to modify: ", "blue")).strip().lower()
+            attribute = input(colored("Enter the attribute to modify: ", "blue")).strip().lower()
+            new_value = input(colored("Enter the new value: ", "blue")).strip().lower()
             database_operations.update_state_attribute(state_name.title(), attribute, new_value)
             
         elif choice == "6":
-            city_name = input("Enter the name of the city to modify: ").strip().lower()
-            attribute = input("Enter the attribute to modify: ").strip().lower()
-            new_value = input("Enter the new value: ").strip().lower()
+            city_name = input(colored("Enter the name of the city to modify: ", "blue")).strip().lower()
+            attribute = input(colored("Enter the attribute to modify: ", "blue")).strip().lower()
+            new_value = input(colored("Enter the new value: ", "blue")).strip().lower()
             database_operations.update_city_attribute(city_name.title(), attribute, new_value)
 
         elif choice == "7":
-            county_name = input("Enter the name of the county to modify: ").strip().lower()
-            attribute = input("Enter the attribute to modify: ").strip().lower()
-            new_value = input("Enter the new value: ").strip().lower()
+            county_name = input(colored("Enter the name of the county to modify: ", "blue")).strip().lower()
+            attribute = input(colored("Enter the attribute to modify: ", "blue")).strip().lower()
+            new_value = input(colored("Enter the new value: ", "blue")).strip().lower()
             database_operations.update_county_attribute(county_name.title(), attribute, new_value)
             
         elif choice == "8":
-            state_name = input("Enter state name to delete: ").strip().lower()
+            state_name = input(colored("Enter state name to delete: ", "blue")).strip().lower()
             database_operations.delete_state_by_name(session, state_name.title())
             
         elif choice == "9":
-            county_name = input("Enter county name to delete: ").strip().lower()
+            county_name = input(colored("Enter county name to delete: ", "blue")).strip().lower()
             database_operations.delete_county_by_name(session, county_name.title())
             
         elif choice == "10":
-            city_name = input("Enter city name to delete: ").strip().lower()
+            city_name = input(colored("Enter city name to delete: ", "blue")).strip().lower()
             database_operations.delete_city_by_name(session, city_name.title())
 
         elif choice == "11":
